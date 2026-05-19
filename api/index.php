2b55bbb2963c8acfc0c7fc11b667eb43607742d6
sidiktsq <baika3542@gmail.com>
2026-05-19 13:26:54 +0700

    load autoload.php before bootstra

--- api/index.php
+++ api/index.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 try {
+    require __DIR__ . '/../vendor/autoload.php';
     $app = require_once __DIR__ . '/../bootstrap/app.php';
     
     // Resolve the HTTP Kernel
