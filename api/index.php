80f6c878156643328e09744748f56b8a72d3b93c
sidiktsq <baika3542@gmail.com>
2026-05-19 13:20:52 +0700

    catch and display Laravel boot exceptions

--- api/index.php
+++ api/index.php
@@ -3,4 +3,11 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Laravel Boot Crash!</h1>";
+    echo "<b>Error Message:</b> " . $e->getMessage() . "<br>";
+    echo "<b>File:</b> " . $e->getFile() . " on line " . $e->getLine() . "<br>";
+    echo "<h3>Stack Trace:</h3><pre>" . $e->getTraceAsString() . "</pre>";
+}
